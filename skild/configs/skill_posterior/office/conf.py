7f3609212d388090f0ef359dad9b3d2fd59e083b
Karl Pertsch <pertsch@usc.edu>
2021-08-04 03:28:16 -0700

    update demo configs

--- skild/configs/skill_posterior/office/conf.py
+++ skild/configs/skill_posterior/office/conf.py
@@ -1,11 +1,10 @@
 from skild.configs.skill_prior.office.conf import *
 
 configuration['data_dir'] = os.path.join(os.environ['DATA_DIR'], 'office_demos')
-data_config.dataset_spec.n_seqs = 100         # number of demos
-data_config.dataset_spec.seq_repeat = 100     # how often to repeat these demos
-data_config.dataset_spec.split = AttrDict(train=0.5, val=0.5, test=0.0)   # use half of the demos for validation
+data_config.dataset_spec.n_seqs = 50         # number of demos
+data_config.dataset_spec.seq_repeat = 3      # how often to repeat these demos
 
-configuration['epoch_cycles_train'] = 200
+configuration['epoch_cycles_train'] = 6000
 
 model_config.embedding_checkpoint = os.path.join(os.environ["EXP_DIR"],
                   "skill_prior/office/office_prior/weights")
